d4f5d7347d2d3de002c76a40426400f065fb93d0
0xJEDI <120197850+0xJEDI@users.noreply.github.com>
2022-12-12 15:04:30 +0000

    Create try_me_task.js

---
+++ functions/OBJECTS/try_me_task.js
@@ -0,0 +1,17 @@
+let day = "Monday";
+
+const person = {
+    firstName: "Sally",
+    lastName: "Evans",
+    age: 27,
+    occupation: "Sales Assistant",
+    married: false,
+    homeOwner: false,
+    hobbies: ["Tennis", "Football", "Running"],
+}
+
+Object.property
+person.firstName
+console.log(person.firstName)
+
+console.log(person["firstName"])

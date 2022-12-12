342c2926eaad1948770c381729994d482af749da
0xJEDI <120197850+0xJEDI@users.noreply.github.com>
2022-12-12 14:37:30 +0000

    Create music.js

---
+++ If Else Switch/IF_STATEMENT/music.js
@@ -0,0 +1,12 @@
+//ACTIVITY 1
+let music = "Classical"
+
+if (music == "Classical") {
+    console.log("Oh, How Relaxing")
+} else if (music == "Disco") {
+    console.log("Where are my dancing shoes!")
+} else if (music == "Dance") {
+    console.log("Where's the party")
+} else {
+    console.log("Turn it down!")
+}

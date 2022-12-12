04534b0ec1aa909ebf32e5de273bb2ea8b7da084
0xJEDI <120197850+0xJEDI@users.noreply.github.com>
2022-12-12 14:30:23 +0000

    Create loops.js

---
+++ loops/FOR_LOOPS/loops.js
@@ -0,0 +1,8 @@
+console.clear()
+
+// ACTIVITY 1
+let favHolidayPlaces = ["Venice, Italy", "Paris, France", "Salou, Spain"]
+
+for (let i = 0; i < favHolidayPlaces.length; i++){
+    console.log(favHolidayPlaces[i])
+}
